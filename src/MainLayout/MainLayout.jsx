d51690a38efbfdc806f3ad66b4a7841cba6a1186
Raihan <aboraihan14@gmail.com>
2024-12-17 00:27:08 +0600

    navbar added and working on register form

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../Common/Navbar/Navbar'
+import Footer from '../Common/Footer/Footer'
+
+const MainLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto' >
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
+
+export default MainLayout
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../Main/Main";
 import Home from "../Pages/Home";
+import MainLayout from "../MainLayout/MainLayout";
+import Register from "../Components/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main />,
+    element: <MainLayout />,
     children: [
       {
         path: "/",
         element: <Home />
+      },      
+      {
+        path: "/register",
+        element: <Register />
       }
     ]
   },
